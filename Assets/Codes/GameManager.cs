93c0fcfae117c40cbbe57607c6314167d037180a
jiho222 <jhryooz2@gmail.com>
2024-08-01 06:23:19 +0900

    조이스틱까지 완료

--- Assets/Codes/GameManager.cs
+++ Assets/Codes/GameManager.cs
@@ -147,14 +147,14 @@ public class GameManager : MonoBehaviour
     {
         isLive = false;
         Time.timeScale = 0;
-        // uiJoy.localScale = Vector3.zero;
+        uiJoy.localScale = Vector3.zero;
     }
 
     public void Resume()
     {
         isLive = true;
         Time.timeScale = 1;
-        // uiJoy.localScale = Vector3.one;
+        uiJoy.localScale = Vector3.one;
     }
 
     void BossSpawn()
